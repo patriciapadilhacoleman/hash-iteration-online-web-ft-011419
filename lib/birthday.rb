5eb02638b1278a5786f14a71b5bc94a069be2745
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-15 19:55:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,11 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  winner = ""
+  passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
 
 
